f7abe59ef939b7fb32f3c06525e1af5fb18e7f1b
Seeram <BSEERAM@ford.com>
2018-10-31 18:01:51 +0530

    Please enter the commit message for your changes. Lines starting

--- Calculator/app/src/main/java/com/example/bseeram/calculator/Calculate.java
+++ Calculator/app/src/main/java/com/example/bseeram/calculator/Calculate.java
@@ -6,34 +6,78 @@ import java.util.Stack;
 
 public class Calculate {
 
-    String result,append="";
+    double resultDouble;
+    String append="";
     Set<String> numbers = new HashSet<>();
     Set<String> operators = new HashSet<>();
     Stack<String> stack = new Stack<>();
 
     public String resultCalculator(String clickedValue){
 
+        double op1=0.0,op2;
         operators.add("+");
         operators.add("-");
         operators.add("*");
         operators.add("/");
         numbers.add("0");
+        numbers.add("1");
+        numbers.add("2");
+        numbers.add("3");
+        numbers.add("4");
+        numbers.add("5");
+        numbers.add("6");
+        numbers.add("7");
+        numbers.add("8");
+        numbers.add("9");
 
         if (numbers.contains(clickedValue))
         {
-            if (stack.empty())
+            if (stack.empty() || operators.contains(stack.peek()))
             {
                 stack.push(clickedValue);
                 return clickedValue;
             }
             else
             {
-                append=stack.peek()+clickedValue;
-                stack.push(clickedValue);
+                append=stack.pop()+clickedValue;
+                stack.push(append);
             }
             return append;
         }
-        else
-            return "";
+        else if (operators.contains(clickedValue))
+        {
+            stack.push(clickedValue);
+            return clickedValue;
+        }
+        else if (clickedValue.equals("="))
+        {
+            op2 = Double.parseDouble(stack.pop());
+            switch (stack.pop())
+            {
+                case "+":
+                    op1 = Double.parseDouble(stack.pop());
+                    resultDouble = op1+op2;
+                    stack.push(Double.toString(resultDouble));
+                    return Double.toString(resultDouble);
+                case "-":
+                    op1 = Double.parseDouble(stack.pop());
+                    resultDouble = op1-op2;
+                    stack.push(Double.toString(resultDouble));
+                    return Double.toString(resultDouble);
+                case "*":
+                    op1 = Double.parseDouble(stack.pop());
+                    resultDouble = op1*op2;
+                    stack.push(Double.toString(resultDouble));
+                    return Double.toString(resultDouble);
+                case "/":
+                    op1 = Double.parseDouble(stack.pop());
+                    resultDouble = op1/op2;
+                    stack.push(Double.toString(resultDouble));
+                    return Double.toString(resultDouble);
+            }
+        }
+        else if (clickedValue.equals("C"))
+            stack.pop();
+        return "";
     }
 }
